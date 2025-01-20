55898b3428f3108fa17409a3d324974a07ce5b24
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-01-20 20:59:35 +0100

    trying to get images

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { APP_URL, CARDS_PATH } from './constants'
+
+const imgPath = CARDS_PATH
+// let fullArray = []
+const getImages = async (count = 4) => {
+	const images = Object.keys(import.meta.glob('./assets/cards/*.{png,jpeg,jpg,svg}'))
+	console.log(images)
+	const imagesPaths = Object.entries(images)
+	console.log(imagesPaths)
+	const shuffledPaths = imagesPaths.sort(() => Math.random() - 0.5)
+	const selectedPaths = shuffledPaths.slice(0, count)
+
+	// const loadedImages = selectedPaths.map(path => path[0])
+
+	// console.log(selectedPaths)
+	// console.log(loadedImages)
+	let fullArray = selectedPaths.flatMap(item => {
+		const filename = item[0].split('/').pop() //get the file name
+		const id = filename.split('.')[0] //extract the identifier before the extension
+		return [
+			{ id: `${id}-n1`, path: item[0] }, //add a path for each copy
+			{ id: `${id}-n2`, path: item[0] }
+		]
+	})
+
+	console.log(fullArray)
+	fullArray = fullArray.sort(() => Math.random() - 0.5)
+	console.log(fullArray)
+	// console.log(loadedImages)
+	return fullArray
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [cards, setCards] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		const fetchImages = async () => {
+			const images = await getImages()
+			setCards(images)
+		}
+		fetchImages()
+	}, [])
+
+	return (
+		<>
+			<div className='card'>
+				{cards.map(card => (
+					<img
+						key={card.id}
+						src={`${APP_URL}/src/${card.path}`}
+						width={200}
+						alt=''
+					/>
+				))}
+			</div>
+		</>
+	)
 }
 
 export default App
