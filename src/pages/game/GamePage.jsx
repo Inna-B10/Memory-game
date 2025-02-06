9355da01711d6b5a70f99608cfd431391ee52832
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-02-06 22:01:31 +0100

    *

--- src/pages/game/GamePage.jsx
+++ src/pages/game/GamePage.jsx
@@ -105,43 +105,6 @@ export function GamePage() {
 		openEndGame,
 		currentUser
 	})
-	//
-	// 	const prevResultsRef = useRef(null) // store old results
-	// 	const messageRef = useRef(null) // store message
-	//
-	// 	//if prevResultsRef is empty,
-	// 	//save old results ONLY ONCE (at first render)
-	// 	useEffect(() => {
-	// 		if (!prevResultsRef.current && currentUser?.results)
-	// 			prevResultsRef.current = { ...currentUser.results }
-	// 	})
-	//
-	// 	useEffect(() => {
-	// 		if (matchedCards.length && matchedCards.length === cards.length / 2) {
-	// 			stopGame()
-	//
-	// 			//check if is new record based on prevResultsRef
-	// 			const isNewRecord =
-	// 				prevResultsRef.current[level].moves === 0 ||
-	// 				countMoves < prevResultsRef.current[level].moves ||
-	// 				(countMoves === prevResultsRef.current[level].moves &&
-	// 					gameDuration < prevResultsRef.current[level].time)
-	//
-	// 			//Create message and save it in ref
-	// 			messageRef.current = isNewRecord ? (
-	// 				<NewScore
-	// 					moves={countMoves}
-	// 					time={gameDuration}
-	// 				/>
-	// 			) : null
-	//
-	// 			updateUser(countMoves, level, gameDuration)
-	//
-	// 			setTimeout(() => {
-	// 				openEndGame(messageRef.current)
-	// 			}, 1000)
-	// 		}
-	// 	}, [matchedCards, cards, updateUser, countMoves, level, stopGame, gameDuration, openEndGame])
 
 	/* ------------------------- Processing Card Clicks ------------------------- */
 	const turnCard = useCallback(
